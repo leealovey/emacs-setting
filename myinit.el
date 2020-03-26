84c76ac3ca45f52f521590ecd9300ce74144b6e8
Lei Wang <wl1434351332@outlook.com>
2020-03-26 21:30:35 +0800

    add irony markdown-mode auctex

--- myinit.el
+++ myinit.el
@@ -358,3 +358,81 @@ mode-enable))
 ("C-c m c" . magit-commit))
 )
 ;; Magit:1 ends here
+
+;; [[file:~/.emacs.d/myinit.org::*Rust][Rust:1]]
+(use-package rust-mode
+:ensure t)
+
+(use-package racer
+:ensure t)
+;; Rust:1 ends here
+
+;; [[file:~/.emacs.d/myinit.org::*Yaml%20mode][Yaml mode:1]]
+(use-package yaml-mode
+:ensure t)
+;; Yaml mode:1 ends here
+
+;; [[file:~/.emacs.d/myinit.org::*Web%20mode][Web mode:1]]
+(use-package web-mode
+:ensure t
+:config
+(progn
+
+(add-to-list 'auto-mode-alist '("\\.phtml\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.tpl\\.php\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.[agj]sp\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.as[cp]x\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.erb\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.mustache\\'" . web-mode))
+(add-to-list 'auto-mode-alist '("\\.djhtml\\'" . web-mode))
+
+(setq web-mode-ac-sources-alist
+  '(("php" . (ac-source-yasnippet ac-source-php-auto-yasnippets))
+    ("html" . (ac-source-emmet-html-aliases ac-source-emmet-html-snippets))
+    ("css" . (ac-source-css-property ac-source-emmet-css-snippets))))
+
+(add-hook 'web-mode-before-auto-complete-hooks
+          '(lambda ()
+             (let ((web-mode-cur-language
+                    (web-mode-language-at-pos)))
+               (if (string= web-mode-cur-language "php")
+                   (yas-activate-extra-mode 'php-mode)
+                 (yas-deactivate-extra-mode 'php-mode))
+               (if (string= web-mode-cur-language "css")
+                   (setq emmet-use-css-transform t)
+		 (setq emment-use-css-transform nil)))))
+))
+;; Web mode:1 ends here
+
+;; [[file:~/.emacs.d/myinit.org::*Markdown%20mode][Markdown mode:1]]
+(use-package markdown-mode
+:ensure t
+:mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+:init (setq markdown-command "multimarkdown"))
+;; Markdown mode:1 ends here
+
+;; [[file:~/.emacs.d/myinit.org::*Auctex][Auctex:1]]
+(use-package tex-site
+:defer t
+:ensure auctex
+:config
+(setq TeX-auto-save t))
+;; Auctex:1 ends here
+
+;; [[file:~/.emacs.d/myinit.org::*Irony][Irony:1]]
+(use-package irony
+:ensure t
+:config
+(progn
+  (add-hook 'c++-mode-hook 'irony-mode)
+  (add-hook 'c-mode-hook 'irony-mode)
+  (add-hook 'objc-mode-hook 'irony-mode)
+
+  (add-hook 'irony-mode-hook 'irony-cdb-autosetup-compile-options)
+))
+
+(use-package flycheck-irony
+:ensure t)
+;; Irony:1 ends here
